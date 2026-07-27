96f0f0104b68d5d554f2461201737b771482e152
Varun Kumar Khare <vkhare@netskope.com>
2026-07-27 13:19:41 +0530

    Fix biome format drift in parent-session test

--- packages/coding-agent/test/session/session-manager-parent-session.test.ts
+++ packages/coding-agent/test/session/session-manager-parent-session.test.ts
@@ -1,10 +1,7 @@
 import { describe, expect, it } from "bun:test";
 import * as fs from "node:fs/promises";
 import * as path from "node:path";
-import {
-	CURRENT_SESSION_VERSION,
-	type SessionHeader,
-} from "@oh-my-pi/pi-coding-agent/session/session-entries";
+import { CURRENT_SESSION_VERSION, type SessionHeader } from "@oh-my-pi/pi-coding-agent/session/session-entries";
 import { loadEntriesFromFile } from "@oh-my-pi/pi-coding-agent/session/session-loader";
 import { SessionManager } from "@oh-my-pi/pi-coding-agent/session/session-manager";
 import { TempDir } from "@oh-my-pi/pi-utils";
